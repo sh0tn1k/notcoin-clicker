a52d8f4e7c032cee5d3a54fcf443b99db46c29c2
Artem Kondratiev <a1420276@gmail.com>
2024-06-19 00:20:05 +0300

    Init commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Notcoin } from './notcoin'
+
+import './index.css'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+root.render(
+  <React.StrictMode>
+    <main>
+      <Notcoin 
+        canIClickPlease={true}
+        sleep={false}
+        funMode={true}
+        clickValue={1}
+        cooldown={0}
+        handleClick={() => {/*nothing*/}}
+      />
+    </main>
+  </React.StrictMode>
+)
